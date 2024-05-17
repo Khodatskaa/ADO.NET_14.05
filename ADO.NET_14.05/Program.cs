b15e8bf45f68dfbd638aadbac940c2d8ffa30a3d
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-05-17 14:06:29 +0300

    Lab was done

--- ADO.NET_14.05/Program.cs
+++ ADO.NET_14.05/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace ADO.NET_14._05
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            using (var context = new GameContext())
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+
+                var games = new[]
+                {
+                    new Game { Name = "Game 1", Studio = "Studio A", Style = "Style1", ReleaseDate = new DateTime(2020, 1, 1) },
+                    new Game { Name = "Game 2", Studio = "Studio B", Style = "Style2", ReleaseDate = new DateTime(2021, 5, 20) },
+                    new Game { Name = "Game 3", Studio = "Studio C", Style = "Style3", ReleaseDate = new DateTime(2019, 8, 15) }
+                };
+
+                context.Games.AddRange(games);
+                context.SaveChanges();
+
+                var allGames = context.Games.ToList();
+
+                foreach (var game in allGames)
+                {
+                    Console.WriteLine($"Name: {game.Name}, Studio: {game.Studio}, Style: {game.Style}, Release Date: {game.ReleaseDate.ToShortDateString()}");
+                }
+            }
         }
     }
 }
